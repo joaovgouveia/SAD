ab0dbac2c5f3dd97472028f8b796d81ace56a673
jotav <joaovictorgn2004@gmail.com>
2024-08-29 20:29:38 -0300

    Implementa Lógica de diagnósticos

--- SAD/app/Diagnosis/Diagnosis.hs
+++ SAD/app/Diagnosis/Diagnosis.hs
@@ -3,30 +3,20 @@ module Diagnosis.Diagnosis where
 
 import Data.Aeson (ToJSON, FromJSON, decode)
 import Data.Maybe (fromMaybe)
+import Data.Function ( on )
 import qualified Data.ByteString.Lazy as B
+import Utils.Utils (readJsonFile, belongs, intersect, intersectionList)
 import Diseases.Disease (Disease(..))
 import Medications.Medication(Medication(..))
-import Prescriptions.PrescriptionsController (symptons)
-import Diseases.DiseasesController(formatDisease)
+import qualified Diseases.DiseasesController as DC
 
--- (sintomasAssociados (head diseases))
 findDisease :: [String] -> IO String
 findDisease symptons = do
-    putStrLn (unwords symptons)
     let path = "./Diseases/Diseases.JSON"
     diseases <- fromMaybe [] <$> readJsonFile path
-    putStrLn (unwords (map Diseases.DiseasesController.formatDisease diseases))
-    let filterDiseases = filter (belongs symptons . sintomas_associados) diseases
-    return (unwords (map Diseases.DiseasesController.formatDisease filterDiseases))
-
--- Utility functions
-readJsonFile :: (FromJSON a) => FilePath -> IO (Maybe [a])
-readJsonFile path = do
-    content <- B.readFile path
-    return (decode content)
-
-belongs :: Eq a => [a] -> [a] -> Bool
-belongs (h:t) l
-    | null t && h `elem` l = True
-    | h `elem` l = belongs t l
-    | otherwise = False
+    let filterDiseases = filter (intersect symptons . sintomas_associados) diseases
+    if null filterDiseases then return "Diagnóstico incerto"
+    else return (unwords (map description filterDiseases))
+    where description d = DC.formatDisease d ++ "Probabilidade: " ++ show (calculateProp d) ++ "%\n\n"
+          calculateProp glob = divide (length (intersectionList symptons (sintomas_associados glob))) (length (sintomas_associados glob)) * 100
+          divide = (/) `on` fromIntegral
