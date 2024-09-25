08be7d7857c7bab11adc02363f51e691d019956b
jotav <joaovictorgn2004@gmail.com>
2024-09-24 21:29:53 -0300

    Adds: UserIO

---
+++ SAD-PROLOG/src/UserIO.pl
@@ -0,0 +1,10 @@
+#Trabalhe Luciano
+:- module(user_io, [menu/0, exit_system/0]).
+
+:- use_module("../utils/Utils").
+:- use_module("./Controller").
+
+menu:-
+    write("Opção:\n> "), 
+    read_line_to_string(user_input, Option),
+    run(Option).
