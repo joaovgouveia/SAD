f42de191b518f2de37bdc6448d1a06ce61efde8e
ViniciusI4n <vinicius.ian.alves.lopes@ccc.ufcg.edu.br>
2024-09-28 04:01:24 -0300

    adiciona comentarios de direcionamento de menu de receita

--- SAD-PROLOG/src/prescriptions.pl
+++ SAD-PROLOG/src/prescriptions.pl
@@ -9,6 +9,7 @@
 prescriptions_menu :-
     write("menu de receitas\n").
 
+% Função auxiliar que gera a listagem de sintomas
 list_symptons([], _, []).
 list_symptons([X|Rest], N, [XF|Resp]) :-
     atom_number(Atom,N),
@@ -19,6 +20,7 @@ list_symptons([X|Rest], N, [XF|Resp]) :-
     NN is N + 1,
     list_symptons(Rest, NN, Resp).
 
+% Função principal que lista os sintomas
 enumerate_symptons :-
     read_json("../db/prescriptions.JSON", DadosReceitas),
     findall(Sympton, (member(Doc, DadosReceitas), get_dict(sintoma, Doc, Sympton)), Symptons),
@@ -27,6 +29,7 @@ enumerate_symptons :-
     print_bold(ListSymptons),
     !.
 
+% Função auxiliar que verifica os sintomas passados
 valid_symptons(Symptons) :-
     read_json("../db/prescriptions.JSON", DadosReceitas),
     findall(Sympton, (member(Doc, DadosReceitas), get_dict(sintoma, Doc, Sympton), member(Sympton, Symptons)), ListSymptons),
@@ -34,18 +37,21 @@ valid_symptons(Symptons) :-
     length(Symptons, R1),
     R =:= R1.
 
+% Função auxiliar que gera a receita MÉDICA
 generate_string(Symptons) :-
     format_symptons(Symptons, "", Result),
     print_highlighted("\nRECEITA MÉDICA:\n"),
     print_bold(Result),
     !.
 
+% Função auxiliar que gera a receita MÉDICA
 format_symptons([], Acc, Acc).
 format_symptons([Sympton|Rest], Acc, Result) :-
     format_one_sympton(Sympton, FormattedSympton),
     string_concat(Acc, FormattedSympton, NewAcc),
     format_symptons(Rest, NewAcc, Result).
 
+% Função auxiliar que gera a receita MÉDICA
 format_one_sympton(Sympton, Formatted) :-
     get_dict(medicamento_nome, Sympton, MedNome),
     get_dict(medicamento_dosagem, Sympton, MedDosagem),
@@ -55,6 +61,7 @@ format_one_sympton(Sympton, Formatted) :-
     format(string(Formatted), "\nMedicamento: ~w\nDosagem: ~w\nFrequência: ~w\nTipo: ~w\nObservações: ~w\n",
            [MedNome, MedDosagem, MedFreq, MedTipo, MedObs]).
 
+% Função principal que gera a receita MÉDICA
 generate_prescription(Symptons) :-
     (valid_symptons(Symptons) -> true ; print_error("SINTOMA(S) INFORMADO(S) NÃO CADASTRADO(S)")),
     read_json("../db/prescriptions.JSON", DadosReceitas),
