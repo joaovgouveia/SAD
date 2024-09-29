612bc6ee2a9b803c79d0ee5d520350ed42fd640a
joaov <joao.victor.gouveia.nascimento@ccc.ufcg.edu.br>
2024-09-29 00:09:41 -0300

    Finaliza paciente

--- SAD-PROLOG/src/patients.pl
+++ SAD-PROLOG/src/patients.pl
@@ -9,28 +9,53 @@
 patients_menu :- 
     write("menu de pacientes\n").
 
-create_patient(Id, Name, Age).
+% Cria um paciente
+create_patient(Id, Name, Age):-
     read_json("../db/patients.JSON", Patients),
-    (eh_paciente(Id) -> print_error("PACIENTE JA EXISTE NO SISTEMA."; true)),
-    NewPatient = _{
-        consultas: [],
-        id_patient: Id,
-        idade: Age,
-        nome_patient: Name 
-    },
-    append(Patients, [NewPatient], PatientsNew),
-    print_success("PACIENTE CADASTRADO COM SUCESSO!"),!.
+    (eh_paciente(Id) -> print_error("PACIENTE JA EXISTE NO SISTEMA.\n");
+        NewPatient = _{
+            consultas: [],
+            id_patient: Id,
+            idade: Age,
+            nome_patient: Name 
+        },
+        append(Patients, [NewPatient], PatientsNew),
+        write_json("../db/patients.JSON", PatientsNew),
+        print_success("PACIENTE CADASTRADO COM SUCESSO!\n")),!.
 
+% Atualiza um paciente
 update_patient(Id, Name):-
     read_json("../db/patients.JSON", Patients),
-    (eh_paciente(Id) -> true; print_error("PACIENTE NAO CADASTRADO NO SISTEMA.")),
+    (eh_paciente(Id) -> true; print_error("PACIENTE NAO CADASTRADO NO SISTEMA.\n")),
     select(Patient, Patients, Rest),
     get_dict(id_patient, Patient, Id),
     put_dict(nome_patient, Patient, Name, NewPatient),
     append(Rest, [NewPatient], NewData),
-    print_success("PACIENTE ATUALIZADO COM SUCESSO\n"),
-    write_json("../db/patients.JSON", NewData),
-    !.
+    print_success("PACIENTE ATUALIZADO COM SUCESSO!\n"),
+    write_json("../db/patients.JSON", NewData),!.
 
-read_patient(Id).
-delete_patient(Id).
+% Encontra um paciente
+read_patient(Id):-
+    read_json("../db/patients.JSON", Patients),
+    (eh_paciente(Id) -> true; print_error("PACIENTE NAO CADASTRADO NO SISTEMA.\n")),
+    select(Patient, Patients, _),
+    get_dict(id_patient, Patient, Id),
+    get_dict(nome_patient, Patient, Name),
+    get_dict(idade, Patient, Age),
+    get_dict(consultas, Patient, Appointments),
+    string_concat("Nome: ", Name, Header),
+    string_concat("\nIdade: ", Age, Temp),
+    string_concat(Temp, "\nConsultas: ", Text),
+    print_success(Header),
+    print_bold(Text),
+    print_bold(Appointments),
+    write("\n"),!.
+    
+% Deleta um paciente
+delete_patient(Id):-
+    read_json("../db/patients.JSON", Patients),
+    (eh_paciente(Id) -> true; print_error("PACIENTE NAO CADASTRADO NO SISTEMA.\n")),
+    select(Patient, Patients, Rest),
+    get_dict(id_patient, Patient, Id),
+    print_success("PACIENTE DELETADO COM SUCESSO!\n"),
+    write_json("../db/patients.JSON", Rest),!.
