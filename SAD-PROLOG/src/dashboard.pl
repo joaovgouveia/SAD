46e23ecea010c510458429761fd0c7041a4103d5
joaov <joao.victor.gouveia.nascimento@ccc.ufcg.edu.br>
2024-10-01 20:57:49 -0300

    Atualiza: dashboard (só falta principais doenças)

--- SAD-PROLOG/src/dashboard.pl
+++ SAD-PROLOG/src/dashboard.pl
@@ -19,8 +19,13 @@ most_appointments_header :-
     print_spacer, print_bold("          Médico(s) com mais consultas\n"), print_spacer.
 
 system_counts_field :-
-    print_error("Quantidade Médicos: [implementar]\nQuantidade Pacientes: [implementar]\n").
-
+    count_doctors(DoctorCount),
+    count_patients(PatientCount),
+    print_bold("Quantidade Médicos: "),
+    print_highlighted(DoctorCount),
+    print_bold("\nQuantidade Pacientes: "),
+    print_highlighted(PatientCount),
+    write("\n").
 
 trending_diseases_header :-
     print_spacer, print_bold("                Principais Doenças\n"), print_spacer.
@@ -32,6 +37,7 @@ trending_diseases_field :-
 show_trending_diseases :-
     print_error("[implementar]").
 
+
 most_appointments_field :-
     most_appointments_header,
     read_json("../db/users.JSON", Users),
@@ -39,6 +45,18 @@ most_appointments_field :-
     max_appointments(Doctors, Value),
     most_appointments(Doctors, Value, R),
     print_all_doctors(R).
+
+count_doctors(Count) :-
+    read_json("../db/users.JSON", Users),
+    select_doctors(Users, Doctors),
+    length(Doctors, Count),
+    print_bold(Count).
+
+count_patients(Count) :-
+    read_json("../db/patients.JSON", Patients),
+    length(Patients, Count),
+    print_bold(Count).
+
 
 most_appointments([], _, []).
 most_appointments([H|T], Value, Doctors) :-
